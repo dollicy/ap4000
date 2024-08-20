a161c19eeebbe159133106a1ba3b8fea5a3654b4
dollicy <fredrick000114@gmail.com>
2024-08-20 05:29:01 +0000

    2

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [`7f73593f318a5f6b8556a47491a7a2bc9d642261b523ab8b2c9a5ca7023cb2ba`],
+      accounts: [``],
     },
   },
 };
